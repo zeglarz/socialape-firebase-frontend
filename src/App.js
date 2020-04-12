c77737bbcec9a384546aee72cf300874b380a964
Konrad Rudnicki <zeglarz@gmail.com>
2020-04-12 19:46:55 +0200

    give file sctructure plus boilerplate and imports

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+// Pages
+
+import Home from './pages/home';
+import Login from './pages/login';
+import Signup from './pages/signup';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Router>
+                <Switch>
+                    <Route exact path='/' component={Home}/>
+                    <Route exact path='/login' component={Login}/>
+                    <Route exact path='/signup' component={Signup}/>
+                </Switch>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/navbar.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+// MUI stuff
+import AppBar from '@material-ui/core/AppBar'
+
+class Navbar extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default Navbar;
