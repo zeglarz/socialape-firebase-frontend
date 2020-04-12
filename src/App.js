e73c66dedccf38e114a05900cb6c74d7dfa82a8c
Konrad Rudnicki <zeglarz@gmail.com>
2020-04-13 01:02:43 +0200

    add theme properties

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
+import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
 
 // Pages
 
@@ -8,17 +10,42 @@ import Home from './pages/home';
 import Login from './pages/login';
 import Signup from './pages/signup';
 
+// Components
+import Navbar from './components/navbar';
+
+const theme = createMuiTheme({
+    palette: {
+        primary: {
+            light: '#33c9dc',
+            main: '#00bcd4',
+            dark: '#008394',
+            contrastText: '#fff'
+        },
+        secondary: {
+            light: '#ff6333',
+            main: '#ff3d00',
+            dark: '#b22a00',
+            contrastText: '#fff'
+        }
+    }
+});
+
 function App() {
     return (
-        <div className="App">
-            <Router>
-                <Switch>
-                    <Route exact path='/' component={Home}/>
-                    <Route exact path='/login' component={Login}/>
-                    <Route exact path='/signup' component={Signup}/>
-                </Switch>
-            </Router>
-        </div>
+        <MuiThemeProvider theme={theme}>
+            <div className="App">
+                <Router>
+                    <Navbar/>
+                    <div className="container">
+                        <Switch>
+                            <Route exact path='/' component={Home}/>
+                            <Route exact path='/login' component={Login}/>
+                            <Route exact path='/signup' component={Signup}/>
+                        </Switch>
+                    </div>
+                </Router>
+            </div>
+        </MuiThemeProvider>
     );
 }
 
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
-
+import { Link } from 'react-router-dom';
 // MUI stuff
-import AppBar from '@material-ui/core/AppBar'
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Button from '@material-ui/core/Button';
 
 class Navbar extends Component {
     render() {
         return (
-            <div>
-                
-            </div>
+            <AppBar>
+                <Toolbar className='nav-container'>
+                    <Button color='inherit' component={Link} to='/login'>Login</Button>
+                    <Button color='inherit' component={Link} to='/'>Home</Button>
+                    <Button color='inherit' component={Link} to='/signup'>Signup</Button>
+                </Toolbar>
+            </AppBar>
         );
     }
 }
