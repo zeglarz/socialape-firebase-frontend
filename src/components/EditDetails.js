9b1c99a6fdea07961eab37526ebbd5f2f655634e
Konrad Rudnicki <zeglarz@gmail.com>
2020-04-17 19:15:41 +0200

    logged user navbar

--- src/components/EditDetails.js
+++ src/components/EditDetails.js
@@ -12,7 +12,6 @@ import TextField from '@material-ui/core/TextField';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import IconButton from '@material-ui/core/IconButton';
 import Button from '@material-ui/core/Button';
@@ -21,7 +20,11 @@ import Button from '@material-ui/core/Button';
 import EditIcon from '@material-ui/icons/Edit';
 
 const styles = theme => ({
-    ...theme.spreadThis
+    ...theme.spreadThis,
+    button: {
+        float: 'right'
+    }
+
 });
 
 class EditDetails extends Component {
@@ -60,6 +63,7 @@ class EditDetails extends Component {
             website
         };
         this.props.editUserDetails(userDetails);
+        this.handleClose();
     };
 
     componentDidMount() {
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -5,18 +5,34 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Button from '@material-ui/core/Button';
 
+// Redux stuff
+import { connect } from 'react-redux';
+
 class Navbar extends Component {
     render() {
+        const { authenticated } = this.props;
         return (
             <AppBar>
                 <Toolbar className='nav-container'>
-                    <Button color='inherit' component={Link} to='/login'>Login</Button>
-                    <Button color='inherit' component={Link} to='/'>Home</Button>
-                    <Button color='inherit' component={Link} to='/signup'>Signup</Button>
+                    {authenticated ? (
+                        <Button color='inherit' component={Link} to='/post'>Add post</Button>
+
+                    ) : (
+                        <>
+                            <Button color='inherit' component={Link} to='/login'>Login</Button>
+                            <Button color='inherit' component={Link} to='/'>Home</Button>
+                            <Button color='inherit' component={Link} to='/signup'>Signup</Button>
+                        </>
+                    )}
+
                 </Toolbar>
             </AppBar>
         );
     }
 }
 
-export default Navbar;
+const mapStateToProps = state => ({
+    authenticated: state.user.authenticated
+});
+
+export default connect(mapStateToProps)(Navbar);
