bcda9942b4063c6b89718afae54fb230642e646c
Konrad Rudnicki <zeglarz@gmail.com>
2020-04-18 16:57:00 +0200

    get all screams action

--- src/redux/actions/dataActions.js
+++ src/redux/actions/dataActions.js
@@ -1,1 +1,26 @@
+import { SET_SCREAMS, LOADING_DATA, LIKE_SCREAM, UNLIKE_SCREAM } from '../types';
+import axios from 'axios';
+
+// Get all screams
+export const getScreams = () => dispatch => {
+    dispatch({ type: LOADING_DATA });
+    axios.get('/api/screams')
+        .then(res => {
+            dispatch({
+                type: SET_SCREAMS,
+                payload: res.data
+            });
+        })
+        .catch(() => {
+            dispatch({
+                type: SET_SCREAMS,
+                payload: {}
+            });
+        });
+};
+
+// Like a scream
+
+
+// Unlike a scream
 
