e7a21ab7f94fb929fde124c5e44aa31202e81c11
Konrad Rudnicki <zeglarz@gmail.com>
2020-04-21 20:22:56 +0200

    adding comment boilerplate

--- src/components/scream/CommentForm.js
+++ src/components/scream/CommentForm.js
@@ -1,7 +1,25 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import withStyles from '@material-ui/core/styles/withStyles';
+
+// MUI Stuff
+import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
+import TextField from '@material-ui/core/TextField';
+
+// Redux stuff
+import { connect } from 'react-redux';
+import { submitComment } from '../../redux/actions/dataActions';
+
+const styles = theme => ({
+    ...theme.spreadThis
+});
 
 class CommentForm extends Component {
+    state = {
+        body: ''
+    };
+
     render() {
         return (
             <div>
@@ -11,6 +29,13 @@ class CommentForm extends Component {
     }
 }
 
-CommentForm.propTypes = {};
-
-export default CommentForm;
+CommentForm.propTypes = {
+    UI: PropTypes.object.isRequired,
+    submitComment: PropTypes.func.isRequired,
+    authenticated: PropTypes.bool.isRequired
+};
+const mapStateToProps = state => ({
+    UI: state.UI,
+    authenticated: state.user.authenticated
+});
+export default connect(mapStateToProps, { submitComment })(withStyles(styles)(CommentForm));
--- src/redux/reducers/dataReducers.js
+++ src/redux/reducers/dataReducers.js
@@ -5,7 +5,8 @@ import {
     LOADING_DATA,
     DELETE_SCREAM,
     POST_SCREAM,
-    SET_SCREAM
+    SET_SCREAM,
+    SUBMIT_COMMENT
 } from '../types';
 
 const initialState = {
@@ -54,6 +55,14 @@ export default function (state = initialState, action) {
             return {
                 ...state,
                 scream: action.payload
+            };
+        case SUBMIT_COMMENT:
+            return {
+                ...state,
+                scream: {
+                    ...state.scream,
+                    comments: [action.payload, ...state.scream.comments]
+                }
             };
         default:
             return state;
